49b3b9ef5ea1543b1d931615f28646e0f538dd7b
ayaankhan28 <98885614+ayaankhan28@users.noreply.github.com>
2024-01-17 22:40:02 +0530

    Update gemini3.py

--- gemini3.py
+++ gemini3.py
@@ -5,28 +5,7 @@ import google.generativeai as genai
 import numpy as np
 
 
-
-
 genai.configure(api_key='******')
-
-#####################TO CHECK AVAILABLE MODELS
-"""for m in genai.list_models():
-  if 'generateContent' in m.supported_generation_methods:
-    print(m.name)"""
-
-########################################################################## FOR TEXT TO TEXT
-
-"""model = genai.GenerativeModel('gemini-pro')
-
-
-response = model.generate_content("give me 5 points to start youtube channel")
-print(response.text)"""
-#############FOR ANSWERSS IN CHUNKS
-"""for chunk in response:
-  print(chunk.text)
-  print("_"*80)"""
-##############################################################################
-############################################################################## FOR  IMAGE TO TEXT
 
 import PIL.Image
 def analyze(path,query):
@@ -39,26 +18,7 @@ def analyze(path,query):
     b = time.time()
 
     return response.text
-############################################################################## FOR CHATTING
 
-"""model = genai.GenerativeModel('gemini-pro')
-chat = model.start_chat(history=[])
-while True:
-
-    prompt = input("Enter your question: ")
-    a = time.time()
-    if(prompt == "exit"):
-        break
-    else:
-        response = chat.send_message(f"{prompt}")
-        output=chat.history
-        ##print(output)
-        print(response.text)
-        b = time.time()
-        #speech.speak(response.text)
-
-        print("Response Time: "+str(round(b-a))+" sec")"""
-#speechV2.speak(output)
 
 
 

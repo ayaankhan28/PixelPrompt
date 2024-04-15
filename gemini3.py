ebd2e907089079caa940e766b2544455eea65f54
ayaankhan28 <98885614+ayaankhan28@users.noreply.github.com>
2024-04-15 15:09:11 +0530

    Update gemini3.py

--- gemini3.py
+++ gemini3.py
@@ -1,28 +1,13 @@
 import textwrap
 import time
-
 import google.generativeai as genai
 import numpy as np
-
-
 genai.configure(api_key='******')
-
 import PIL.Image
 def analyze(path,query):
     img = PIL.Image.open(f"{path}")
-
     a = time.time()
     model = genai.GenerativeModel('gemini-pro-vision')
-
     response = model.generate_content([f"{query}",img])
     b = time.time()
-
     return response.text
-
-
-
-
-"""
-
-
-"""
